1e5e01cf0a1661be9d7a07d110c2ef0bc512cc41
PlaneCargo191 <t.james_17@hotmail.com>
2021-08-29 18:16:11 +0700

    DONE

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About Me</p>
+            <p className='text-xl'>Thanakorn Chaininphun 620610790</p>
+        </div>
+    )
+}
+
+export default AboutMe
